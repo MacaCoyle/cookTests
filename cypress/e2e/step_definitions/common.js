b49051b46b12d9fd2ce4496564f6df1079bbf9cc
MacaCoyle <38429125+MacaCoyle@users.noreply.github.com>
2025-01-20 10:40:34 -0300

    Update common.js

--- cypress/e2e/step_definitions/common.js
+++ cypress/e2e/step_definitions/common.js
@@ -1,6 +1,4 @@
 import { Given } from "@badeball/cypress-cucumber-preprocessor";
-
-
 Given(`I am on the {string} homepage`, (urlName) =>{
     const url = Cypress.env(`${urlName.toUpperCase()}_URL`);
     cy.visit(url);

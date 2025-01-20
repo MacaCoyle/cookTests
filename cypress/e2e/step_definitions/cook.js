1c6a76630a715b5d3311dc9d6d1e739148ed060f
MacaCoyle <38429125+MacaCoyle@users.noreply.github.com>
2025-01-20 10:43:25 -0300

    Update cook.js

--- cypress/e2e/step_definitions/cook.js
+++ cypress/e2e/step_definitions/cook.js
@@ -9,38 +9,38 @@ When(`I enter the zip code {string}`, (zipcode) => {
 });
 
 When(`I skip the quiz`,() => {
-cy.wait(2000)
+ cy.wait(2000)
  cookHome.getSkipAllLink().click({ force: true });
 });
 
 When(`I select the 6 meals plan`, () => {
-    cy.wait(2000)
-    cookHome.getNumbersOfMealsBtn().click({ force: true });
-    cy.wait(2000)
-    cookHome.getContinueBtn().click({ force: true });
-    cy.wait(2000)
+ cy.wait(2000)
+ cookHome.getNumbersOfMealsBtn().click({ force: true });
+ cy.wait(2000)
+ cookHome.getContinueBtn().click({ force: true });
+ cy.wait(2000)
 });
 
 Then(`I fill the input email with {string}`, (email,) => {
-    cy.wait(2000)
-    cookLogin.getLoginLink().click({ force: true });
-    cy.wait(4000)
-    cookLogin.getLoginAccountLink().click({ force: true });
-    cy.wait(4000)
-    cookLogin.getEmailInput().type(email);
-    cy.wait(4000)
+ cy.wait(2000)
+ cookLogin.getLoginLink().click({ force: true });
+ cy.wait(4000)
+ cookLogin.getLoginAccountLink().click({ force: true });
+ cy.wait(4000)
+ cookLogin.getEmailInput().type(email);
+ cy.wait(4000)
 });
 
 Then(`I fill the input password with {string}`, (password) => {
-    cy.wait(4000)
-    cookLogin.getPassworInput().type(password);
-    cy.wait(5000)
+ cy.wait(4000)
+ cookLogin.getPassworInput().type(password);
+ cy.wait(5000)
 });
 
 Then(`I submit login form`, () => {
-    cy.wait(2000)
-    cookLogin.getLoginBtn().click({ force: true });
-    cy.wait(5000)
+ cy.wait(2000)
+ cookLogin.getLoginBtn().click({ force: true });
+ cy.wait(5000)
 });
 
 Then(`the URL should contain {string}`, (mealSelect) => {
